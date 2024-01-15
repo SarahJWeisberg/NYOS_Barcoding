581f1921bce08e0ead0b76e5532421e09f7a5db4
SarahJWeisberg <sarah.j.weisberg@stonybrook.edu>
2024-01-15 11:29:58 -0500

    filling gaps in Lewis dataset, correcting for size-biased subsampling

--- R/Lewis_data.R
+++ R/Lewis_data.R
@@ -1,7 +1,7 @@
 #Code to calculate thermal niches of fish eggs 
 #from EcoMon survey program & barcoding results reported in Lewis et al (2016)
 
-# Mon Nov 27 14:18:41 2023 ------------------------------
+# Mon Jan 15 10:36:51 2024 ------------------------------
 
 # prep --------------------------------------------------------------------
 #load packages
@@ -11,82 +11,129 @@ library(tidyr)
 library(MASS)
 library(here)
 library(ggplot2)
+library(rio)
 
 # Load and tidy data ------------------------------------------------------
 #Load station data from subset of stations with ethanol-preserved samples
-db <- read_excel("data/FishEggDatabase_GenID.accdb.xls",sheet = "StationData")
+db_orig <- read_excel("data/FishEggDatabase_GenID.accdb.xls",sheet = "StationData")
 #remove rows with NA egg count
-#convert egg counts to numeric, station to double
-#rename columns to match ecomon
-db <- db %>% filter(is.na(Egg_Count) == F) %>% 
-  mutate(Egg_Count = as.numeric(Egg_Count),Station=as.double(Station),Ich_Haul_Fctr = as.numeric(Ich_Haul_Fctr)) %>% 
-  rename(cruise_name = Cruise_Name, station = Station)
+#convert egg counts to numeric, create new column with cruise/station unique ID
+db <- db_orig %>% filter(is.na(Egg_Count) == F) %>% 
+  mutate(Egg_Count = as.numeric(Egg_Count),Station=as.double(Station),Ich_Haul_Fctr = as.numeric(Ich_Haul_Fctr)) %>%
+  mutate(cruise_station = paste(Cruise_Name,Station,sep="_"))
 
-#Load all EcoMon station data
-#convert station to double
-ecomon <- read.csv("data/EcoMon_Plankton_Data.csv") %>% mutate(station = as.double(station))
+#read in database with haul factor info
+stations <- read.csv("data/DensitiesBySpecies.csv")
+haul <- stations %>% rename(IHF = Ich_Haul_Fctr) %>% mutate(cruise_station = paste(Cruise_Name,Station,sep="_")) %>%
+  dplyr::select(cruise_station,IHF)
+
+#merge back with egg db, remove rows with haul factor = NA
+#these are from 4 cruises without barcoding results
+db <- left_join(db,haul, by = "cruise_station") %>% dplyr::select(-Ich_Haul_Fctr) %>% filter(is.na(IHF) ==F)
 
 #load in DNA barcoding results
-eggs<- read_excel("data/FishEggDatabase_GenID.accdb.xls", sheet = "EggIDData")
+eggs_orig<- read_excel("data/FishEggDatabase_GenID.accdb.xls", sheet = "EggIDData")
 #rename eggs columns
-eggs<- eggs %>% rename(Sample_ID=`Sample ID`, Common_Name = `Common Name`,cruise_name=Cruise, station=Station)
+eggs<- eggs_orig %>% rename(Sample_ID=`Sample ID`, Common_Name = `Common Name`,Cruise_Name=Cruise) %>%
+  mutate(cruise_station = paste(Cruise_Name,Station,sep="_"))
 #fix errors in common names
 eggs <- eggs %>% mutate(Common_Name = replace(Common_Name,Common_Name == "American","American fourspot flounder")) %>%
   mutate(Common_Name = replace(Common_Name,Common_Name == "Witch Flounder","Witch flounder")) %>%
   mutate(Common_Name = replace(Common_Name,Common_Name == "Yellowtail Flounder","Yellowtail flounder"))
 
-#join datasets, select for relevant columns
-db_trim<- left_join(db,ecomon,by=c("cruise_name","station")) %>% 
-  dplyr::select(cruise_name,station,sfc_temp,date,Egg_Count,Ich_Haul_Fctr)
+#Load station CTD data
+ecomon <- read.csv("data/station_CTD.csv")
+#rename columns, select for stations of interest, filter for columns of interest
+sst <- ecomon %>% rename(Cruise_Name = CRUISE_NAME, Station = STATION, SST = OCTEMPS_SFC_TEMP) %>% 
+  mutate(cruise_station = paste(Cruise_Name,Station,sep="_")) %>% 
+  filter(cruise_station %in% eggs$cruise_station | cruise_station %in% db$cruise_station) %>%
+  dplyr::select(cruise_station,SST)
 
-#replace NA haul factors with average
-db_trim <- db_trim %>% 
-  mutate(Ich_Haul_Fctr = ifelse(is.na(Ich_Haul_Fctr),mean(Ich_Haul_Fctr,na.rm = T),Ich_Haul_Fctr))
+#average SST for stations with more than 1
+sst <- sst %>% group_by(cruise_station) %>% mutate(SST = mean(SST)) %>% distinct()
+
+#join datasets, select for relevant columns, remove NAs
+db_trim<- left_join(db,sst,by="cruise_station") %>% 
+  dplyr::select(cruise_station,SST,Egg_Count,IHF,Cruise_Name,Station) %>% filter(is.na(SST) == F)
 
 #histogram of SST of all stations
-db_trim %>% 
-  ggplot(aes(x=sfc_temp, color=cruise_name, fill=cruise_name,binwidth = 0.5))+
+db_trim %>%
+  ggplot(aes(x=SST, color=Cruise_Name, fill=Cruise_Name,binwidth = 0.5))+
   geom_histogram(alpha=0.5)
 
-#select relevant columns of egg data
-eggs_trim<- eggs %>% dplyr::select(Sample_ID,cruise_name,station,Common_Name) %>% 
-  mutate(station = as.double(station))
-
 #join egg data with temperature data
-egg_temp<-left_join(eggs_trim,db_trim, by = c("cruise_name","station"))
+egg_temp<-left_join(eggs,sst, by = "cruise_station") 
 
+#Dealing with biased subsampling
+#see Lewis et al., 2016 for details
 #What is tally of eggs barcoded per station, compared with egg count?
-station_tally<-eggs_trim %>% group_by(cruise_name,station) %>% tally()
-station_tally <- left_join(db_trim,station_tally,by = c("cruise_name","station")) %>%
-  rename(barcoded_count = n) %>% mutate(barcoded_count = ifelse(is.na(barcoded_count),0,barcoded_count))
-#there are some whole cruises with non-zero egg counts but none barcoded
-#six other stations with this
-#let's ignore those for now
-station_tally <- station_tally %>% filter(!cruise_name %in% c("GU1302","PC1301","HB1202","DE1105","DE1102")) %>% 
-  filter(!(Egg_Count > 0 & barcoded_count == 0)) %>% mutate(diff=Egg_Count-barcoded_count) %>% 
-  mutate(cruise_station = paste(cruise_name,station,sep="_"))
+station_tally<-eggs %>% group_by(cruise_station) %>% tally()
+station_tally <- left_join(station_tally,db_trim,by = "cruise_station") %>%
+  rename(barcoded_count = n) %>% mutate(barcoded_count = ifelse(is.na(barcoded_count),0,barcoded_count)) %>% 
+  mutate(diff=Egg_Count-barcoded_count) %>% dplyr::select(-SST)
+station_tally <- left_join(station_tally, sst, by="cruise_station") %>% filter(is.na(SST) == F)
 
-#Dealing with subsampling issue
-#make new column with cruise and station merged
+#pull out eggs from stations that were subsampled
 subsampled<-station_tally %>% filter(diff>0)
-#need to bin into 0.05mm diameter bins
-
-eggs_sub <-egg_temp %>% mutate(cruise_station = paste(cruise_name,station,sep="_")) %>% 
+eggs_sub <-egg_temp %>% 
   filter(cruise_station %in% subsampled$cruise_station)
 
-eggs_sub <- eggs_sub %>% group_by(cruise_station,Common_Name) %>% tally() %>% rename(abd = n)
-eggs_sub <- left_join(eggs_sub,subsampled,by="cruise_station") %>% 
-  mutate(est = abd/barcoded_count * Egg_Count * Ich_Haul_Fctr) %>% 
-  dplyr::select(Common_Name,cruise_station,sfc_temp,date,est)
+#pull in information about diameter-based subsampling
+#add number of unid'd eggs + id'd per size bin to get total - can then calculate proportionality
+sizes <- dplyr::bind_rows(import_list("data/FishEgg_MsmtData.xlsx",setclass = "tbl"),.id = "sheet")
+sizes <- sizes %>% rename(cruise_station = sheet) %>% mutate(Tot_Frequency = UnID_Frequency + ID_Frequency) %>%
+  dplyr::select(-c(UnID_Frequency,ID_Frequency))
 
-eggs_no_sub <- egg_temp %>% mutate(cruise_station = paste(cruise_name,station,sep="_")) %>% 
-  filter(!cruise_station %in% subsampled$cruise_station) %>% group_by(Common_Name,cruise_station) %>% 
-  tally() %>% rename(abd = n)
+#separate which eggs we have size info for, which not
+eggs_sub_size <- eggs_sub %>% filter(cruise_station %in% sizes$cruise_station)
+eggs_sub_no_size <- eggs_sub %>% filter(!cruise_station %in% sizes$cruise_station)
 
-eggs_no_sub <- left_join(eggs_no_sub,station_tally,by = "cruise_station") %>% 
-  mutate(est = abd * Ich_Haul_Fctr) %>% dplyr::select(Common_Name,cruise_station,sfc_temp,date,est)
+#estimate total number of eggs per species per subsampled station
+eggs_sub_final<-c()
+for (i in 1:length(unique(sizes$cruise_station))){
+  cs<-unique(sizes$cruise_station)[i] #pull out unique station
+  #get barcorded eggs from that station
+  #bin species IDs into size categories
+  eggs_to_bin <- eggs_sub_size %>% filter(cruise_station == cs) %>% mutate(diameter = as.numeric(`Egg Diameter (mm)`)) %>% 
+    mutate(Bin = ceiling(diameter*20)/20) %>% group_by(Common_Name,Bin,cruise_station) %>% tally()
+  #get size bin info from that station
+  sizes_bin <- sizes %>% filter(cruise_station == cs)
+  #join
+  bin_est <- right_join(eggs_to_bin,sizes_bin,by=c("cruise_station","Bin"))
+  bin_est <- bin_est %>% group_by(Bin) %>% mutate(tot_barcoded = sum(n)) %>% ungroup()
+  #deal with nas, have to join with closest bin with barcoding results
+  bin_bad<- bin_est %>% filter(Tot_Frequency >0 & is.na(tot_barcoded)==T)
+  if (length(bin_bad$Bin) >0){
+    for (j in 1:length(bin_bad$Bin)){
+    bin_good<-bin_est %>% filter(is.na(tot_barcoded)==F)
+    bin_est <- bin_good %>% 
+      mutate(Tot_Frequency= ifelse(Bin == bin_good$Bin[which.min(abs(bin_good$Bin-bin_bad$Bin[j]))],
+                                   bin_bad$Tot_Frequency[j]+Tot_Frequency,Tot_Frequency))
+    }
+  }
+  bin_est <- bin_est %>% mutate(est = n*Tot_Frequency/tot_barcoded)
+  eggs_sub_size_est <- bin_est %>% group_by(Common_Name,cruise_station) %>% mutate(tot=sum(est)) %>% 
+    dplyr::select(Common_Name,cruise_station,tot) %>% distinct()
+  eggs_sub_final <- rbind(eggs_sub_final,eggs_sub_size_est)
+}
 
-egg_estimates<- bind_rows(eggs_no_sub,eggs_sub)
+# 11 stations without size info - am going to assume proportionality
+eggs_sub_no_size <- eggs_sub_no_size %>% group_by(cruise_station,Common_Name) %>% tally() %>% rename(abd = n)
+eggs_sub_no_size <- left_join(eggs_sub_no_size,subsampled,by="cruise_station") %>% 
+  mutate(tot = abd/barcoded_count * Egg_Count) %>% 
+  dplyr::select(Common_Name,cruise_station,tot)
+
+eggs_no_sub <- egg_temp %>% filter(!cruise_station %in% subsampled$cruise_station) %>% group_by(Common_Name,cruise_station) %>% 
+  tally() %>% rename(tot = n)
+
+#put it all together
+#remove NAs, replace 0's with 1's
+egg_estimates <- bind_rows(eggs_no_sub,eggs_sub_final,eggs_sub_no_size) %>% filter(is.na(Common_Name)==F) %>%
+  mutate(tot=ifelse(tot==0,1,tot))
+
+#add in SST info
+egg_estimates <- left_join(egg_estimates,sst,by='cruise_station')
+
 
 # Analysis ----------------------------------------------------------------
 #what might niche breadth be?
@@ -125,8 +172,15 @@ silver_niche<-left_join(temp_dist,silver_dist,by="temp_bin") %>%
   mutate(cdf_q_norm = cumsum(q_norm))
 
 ggplot(silver_niche) +
-  geom_line(aes(x=temp_bin,y=cdf_q_norm),color="magenta")
+  geom_line(aes(x=temp_bin,y=cdf_q_norm),color="magenta")+
+  geom_vline(xintercept = 13.75, color= "black")+
+  geom_vline(xintercept = 18.25, color= "black")
 #geom_line(data=cod_niche,aes(x=temp_bin,y=cdf_q_norm),color="navy")
+
+ggplot(silver_niche) +
+  geom_line(aes(x=temp_bin,y=q_norm),color="magenta")
+
+  
 
 silver_NB <- sum(silver_niche$niche)
 
@@ -154,6 +208,8 @@ silver_niche<-left_join(temp_dist,silver_dist,by="temp_bin") %>%
 
 ggplot(silver_niche) +
   geom_line(aes(x=temp_bin,y=cdf_q_norm),color="magenta")+
+  
+ggplot()+
   geom_line(data=gs_flounder_niche,aes(x=temp_bin,y=cdf_q_norm),color="navy")
 
 silver_NB <- sum(silver_niche$niche)
@@ -173,8 +229,8 @@ gs_flounder_dist <- gs_flounder %>% mutate(total = sum(est,na.rm=T)) %>%
   group_by(temp_bin) %>% filter(is.na(temp_bin) == F) %>% 
   mutate(prop_fish=sum(est)/total) %>% dplyr::select(temp_bin,prop_fish) %>% distinct()
 
-ggplot() +
-  geom_line(data=gs_flounder_dist,aes(x=temp_bin,y=prop_fish))
+ggplot(data=gs_flounder_dist, aes(x=temp_bin,y=prop_fish))+
+  geom_line()
 
 gs_flounder_niche<-left_join(temp_dist,gs_flounder_dist,by="temp_bin") %>% 
   mutate(prop_fish=ifelse(is.na(prop_fish),0,prop_fish)) %>%
@@ -182,6 +238,14 @@ gs_flounder_niche<-left_join(temp_dist,gs_flounder_dist,by="temp_bin") %>%
   mutate(q = prop_fish/prop_samples) %>%
   mutate(q_norm = q/sum(q)) %>%
   mutate(cdf_q_norm = cumsum(q_norm))
+
+ggplot() +
+  geom_line(data=gs_flounder_niche,aes(x=temp_bin,y=q_norm),color= "navy")
+
+ggplot() +
+  geom_line(data=gs_flounder_niche,aes(x=temp_bin,y=cdf_q_norm),color= "navy")+
+  geom_vline(xintercept = 16.5) +
+  geom_vline(xintercept = 25.5)
 
 #old way
 gs_flounder<-egg_temp %>% filter(Common_Name == "Gulf Stream flounder")
